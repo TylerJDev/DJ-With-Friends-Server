804c8690341b0cc9fda0fcb905abc712bd5fbc38
Tyler <tylerjdev@gmail.com>
2020-03-01 15:55:31 -0500

    Adjusted redirect_uri

--- build/keys.js
+++ build/keys.js
@@ -5,4 +5,4 @@ Object.defineProperty(exports, "__esModule", {
 });
 var client_id = exports.client_id = process.env.client_id; // Your client id
 var client_secret = exports.client_secret = process.env.client_secret; // Your secret
-var redirect_uri = exports.redirect_uri = 'http://djwithfriends.com';
+var redirect_uri = exports.redirect_uri = 'http://djwithfriends.com/callback';
