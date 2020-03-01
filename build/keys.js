caf2912fe5c3324202a56c1b7cb276e36163f510
Tyler <tylerjdev@gmail.com>
2020-03-01 15:21:28 -0500

    Added keys

--- build/keys.js
+++ build/keys.js
@@ -3,6 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var client_id = exports.client_id = ''; // Your client id
-var client_secret = exports.client_secret = ''; // Your secret
-var redirect_uri = exports.redirect_uri = 'http://localhost:8080/callback';
+var client_id = exports.client_id = process.env.client_id; // Your client id
+var client_secret = exports.client_secret = process.env.client_secret; // Your secret
+var redirect_uri = exports.redirect_uri = 'djwithfriends.com';
--- src/keys.js
+++ src/keys.js
@@ -1,3 +1,3 @@
-export const client_id = ''; // Your client id
-export const client_secret = ''; // Your secret
-export const redirect_uri = 'http://localhost:8080/callback';
+export const client_id = process.env.client_id; // Your client id
+export const client_secret = process.env.client_secret; // Your secret
+export const redirect_uri = 'djwithfriends.com';
