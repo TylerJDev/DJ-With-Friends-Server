c1e6d545ae8c76c917b283b52a0a72439ea89dd0
Tyler <tylerjdev@gmail.com>
2020-12-28 01:14:55 -0500

    Adjusted setTimer to account for other calls

--- src/utils/setTimer.js
+++ src/utils/setTimer.js
@@ -8,8 +8,8 @@ import { globalStore } from '../socket/store/index.js';
 export const setTimer = (time, timeStarted, currentRoom) => {
     return new Promise((resolve) => {
         const timerInterval = setInterval(() => {
-            if (Date.now() >= (timeStarted + time) || (globalStore.rooms[currentRoom] && globalStore.rooms[currentRoom].skipped)) {
-                globalStore.rooms[currentRoom].skipped = false;
+            if (Date.now() >= (timeStarted + time) || (currentRoom !== null && globalStore.rooms[currentRoom] && globalStore.rooms[currentRoom].skipped)) {
+                globalStore.rooms[currentRoom] && (globalStore.rooms[currentRoom].skipped = false);
                 clearInterval(timerInterval);
                 resolve(Date.now());
             }
